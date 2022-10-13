fe2c6b14e6b0211d50fd0d93fb7c19c2a4fc0007
tuanzisugar <a375578236@163.com>
2022-10-13 16:52:52 +0800

    test

--- live2dwidget/autoload.js
+++ live2dwidget/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://github.com/tuanzisugar/live2d/live2dwidget/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/tuanzisugar/live2d@main/live2dwidget/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
